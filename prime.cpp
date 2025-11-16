105c3fbff29e7ba471e348b566877a28cf654957
NTT1906 <nttislinked@gmail.com>
2025-11-16 20:58:14 +0700

    fix and clean up bai1

--- prime.cpp
+++ prime.cpp
@@ -61,12 +61,8 @@ Poly poly_pow_1x(const bui &n) {
 	for (u32 i = 0; i < hb; ++i) {
 		if (get_bit(n, i)) {
 			poly_mul_mod_ip(res, base, n);
-			// printf("%5d: R1: ", i);
-			printBuiA(res.data(), res.size());
 		}
 		poly_sqr_mod_ip(base, n);
-		// printf("%5d: B1: ", i);
-		printBuiA(base.data(), base.size());
 	}
 	return res;
 }
@@ -86,29 +82,12 @@ static void poly_mul_mod_mont_ip(Poly &A, const Poly &B, const MontgomeryReducer
 		b_skips[i] = bui_is0(B[i]);
 	}
 	Poly C{};
-	// Poly cv{};
-	// Poly cv2{};
-	// Poly cC{};
-	// for (int i = 0; i < POLY_R; ++i) {
-		// if (!bui_is0(A[i])) cv[i] = mr.convertOut(A[i]);
-		// if (!bui_is0(B[i])) cv2[i] = mr.convertOut(B[i]);
-	// }
 	for (int i = 0; i < POLY_R; ++i) {
 		if (a_skips[i]) continue;
 		for (int j = 0; j < POLY_R; ++j) {
 			if (b_skips[j]) continue;
 			bui p = mr.multiply(A[i], B[j]);
 			add_true_mod_ip(C[(i + j) % POLY_R], p, mr.modulus);
-			// bui tp = cv[i];
-			// mul_mod_ip(tp, cv2[j], mr.modulus);
-			// if (cmp(mr.convertOut(p), tp) != 0) {
-				// printf("NO MUL: %5d - %5d\n", i, j);
-			// }
-			// add_mod_ip(cC[(i + j) % POLY_R], tp, mr.modulus);
-
-			// if (cmp(mr.convertOut(C[(i + j) % POLY_R]), cC[(i + j) % POLY_R]) != 0) {
-				// printf("NOE SQR: %5d - %5d\n", i, j);
-			// }
 		}
 	}
 	A = C;
@@ -118,36 +97,15 @@ static void poly_sqr_mod_mont_ip(Poly &A, const MontgomeryReducer &mr) {
 	bool a_skips[POLY_R] = {false};
 	for (int i = 0; i < POLY_R; ++i) a_skips[i] = bui_is0(A[i]);
 	Poly C{};
-	// Poly cv{};
-	// Poly cC{};
-	// for (int i = 0; i < POLY_R; ++i) {
-		// if (!bui_is0(A[i])) cv[i] = mr.convertOut(A[i]);
-	// }
 	for (int i = 0; i < POLY_R; ++i) {
 		if (a_skips[i]) continue;
 		for (int j = 0; j < POLY_R; ++j) {
 			if (a_skips[j]) continue;
 			bui p = mr.multiply(A[i], A[j]);
 			add_true_mod_ip(C[(i + j) % POLY_R], p, mr.modulus);
-			// bui tp = cv[i];
-			// mul_mod_ip(tp, cv[j], mr.modulus);
-			// if (cmp(mr.convertOut(p), tp) != 0) {
-				// printf("NO SQR: %5d - %5d\n", i, j);
-			// }
-			// add_mod_ip(cC[(i + j) % POLY_R], tp, mr.modulus);
-			// if (cmp(mr.convertOut(C[(i + j) % POLY_R]), cC[(i + j) % POLY_R]) != 0) {
-				// printf("NOE SQR: %5d - %5d\n", i, j);
-			// }
 		}
 	}
 	A = C;
-}
-
-void printOg(Poly p, const MontgomeryReducer& mr) {
-	for (int i = 0; i < POLY_R; ++i) {
-		if (!bui_is0(p[i])) p[i] = mr.convertOut(p[i]);
-	}
-	printBuiA(p.data(), p.size());
 }
 
 Poly poly_pow_1x_mont(const bui &n) {
@@ -157,24 +115,15 @@ Poly poly_pow_1x_mont(const bui &n) {
 
 	u32 hb = highest_bit(n);
 	for (u32 i = 0; i < hb; ++i) {
-		// if (i == 28) {
-			// printf("My note");
-		// }
 		if (get_bit(n, i)) {
 			poly_mul_mod_mont_ip(res, base, mr);
-			// printf("%5d: R2: ", i);
-			// printOg(res, mr);
 		}
 		poly_sqr_mod_mont_ip(base, mr);
-		// printf("%5d: B2: ", i);
-		// printOg(base, mr);
 	}
-	// printBuiA(res.data(), res.size());
 	// convert result coefficients back to standard form
 	for (int i = 0; i < POLY_R; ++i) {
 		if (!bui_is0(res[i])) res[i] = mr.convertOut(res[i]);
 	}
-	// printBuiA(res.data(), res.size());
 	return res;
 }
 
@@ -182,7 +131,6 @@ Poly poly_pow_1x_mont(const bui &n) {
 static bool aks_like_prime(const bui &n) {
 	if (!get_bit(n, 0)) return false;
 	Poly p = poly_pow_1x_mont(n);
-	// p = poly_pow_1x(n);
 	bui b1 = bui1();
 	if (cmp(p[0], b1) != 0) return false;
 	u32 k;
@@ -226,7 +174,6 @@ int main(int argc, char* argv[]) {
 
 	if (!freopen(argv[1], "r", stdin)) return 1;
 	if (!freopen(argv[2], "w", stdout)) return 1;
-	// if (!freopen("out.txt", "w", stdout)) return 1;
 
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
